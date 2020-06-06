7606553bb47964726383bb8edf4f07b671b7a5cb
samuel-joly <samuel.joly@laplateforme.io>
2020-06-06 05:40:56 +0200

    Added the modal for project and starting of the slider

--- js/script.js
+++ js/script.js
@@ -5,9 +5,11 @@ function get_project_from_type(type) {
 		data:{"action":"get_project_from_type" , "type":type},
 		success:function(data) {
 			data = JSON.parse(data)
-			console.log(data);
+			count = 0
 			data.forEach(function(item, index, arr) {
+				if(count == 6) {return false}
 				$("#project-container").append($(arr[index]))
+				count ++
 				});
 			
 		}
@@ -15,15 +17,27 @@ function get_project_from_type(type) {
 	})
 }
 
+function show_project_toast(target) {
+		toast = $("#toast"+$(target).attr("id"));
+		toast.modal("show");
+}
+
 get_project_from_type()
 
 $(document).ready(function() {
-	
+
 	$("#project-type input").click(function(e){
 		type = $(e.target).attr("id");
 		$("#project-container .project-card").remove();
 		get_project_from_type(type);
 
+	})
+
+	
+	$(document).click(function(e) {
+		if($(e.target).hasClass('project-card') || $(e.target).hasClass('project-body') || $(e.target).hasClass('projectTopImg')) {
+			show_project_toast(e.target);
+		}
 	})
 
 })
--- view/projectView.php
+++ view/projectView.php
@@ -2,12 +2,63 @@
 <div class='col-lg-4 col-md-6 col-sm-6 col-12 row ml-1 mt-1 project-card' >
 	<div style='height:19em;width:100%;background:url("<?=$project["topImg"]?>");background-size:100%;background-repeat:no-repeat;background-position:center;' 
 		class='d-flex justify-content-center align-items-center project-img m-0 p-0 col-12 rounded' style='background:#2c2c2c;'>
-		<div class='project-body col-12 d-flex justify-content-center align-items-center' >
-			<img src='css/assets/projectCover.png' style='height:19em;'/>
+		<div class='project-body col-12 d-flex justify-content-center align-items-center' id='<?=$project["id"]?>'>
+			<img src='css/assets/projectCover.png' style='height:19em;' id='<?=$project["id"]?>' class='projectTopImg'/>
 		</div>
 	</div>
 
 </div>
 
+<div class='modal fade' role='dialog' aria-hidden='true' id='toast<?=$project["id"]?>' aria-labelledby='titletoast<?=$project["id"]?>' style='min-width:100vw;'>
+	<div class="modal-dialog modal-dialog-centered modal-xl" role="document">
+		<div class='modal-content'>
+			<div class='modal-header p-2'>
+				<h5 class='modal-title m-0 p-0' id='titletoast<?=$project["id"]?>'><?=$project["title"]?></h5>
+				<button type="button" class="ml-2 mb-1 close" aria-label="close" data-dismiss='modal'>
+				      <span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class='modal-body'>
+				<div class='container-fluid'>
+					<div class='row d-flex justify-content-center align-items-center'>
+
+						<div class='col-8 carousel slide' id='carouselProject' data-ride='carousel'>
+							<ol class="carousel-indicators">
+								<?php
+								$count = 0;
+								$imgs = [];
+								
+								foreach(scandir("css/assets/projects/projects-slider/".$project["id"]."/") as $img) { 
+									if($count == 0) {
+								?>
+										<li data-target="#carouselProject" data-slide-to="<?=$count?>" class="active"></li> <?php
+									} else { ?>
+										<li data-target="#carouselProject" data-slide-to="<?=$count?>"></li> <?php
+									}
+									if(!is_dir($img)) {
+										$imgs[] .= $img;
+									}
+									$count++;
+								}
+
+								var_dump($imgs);
+
+								?>
+							</ol>
+						</div>
+
+						<div class='col-4'>
+							<?=$project["description"]?>
+						</div>
+
+					</div>
+				</div>
+			</div>
+			<div class='modal-footer'>
+				...
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php $project_text = ob_get_clean(); ?>
