f5b11c37a58434232598d5dd41ee9c2a66e6f957
samuel-joly <samuel.joly@laplateforme.io>
2020-06-12 06:31:08 +0200

    Added language dynamic for the user

--- index.php
+++ index.php
@@ -1,33 +1,18 @@
-<!DOCTYPE html>
+<?php
 
-<html lang='fr'>
-	<head>
-		<?php include("header.php"); ?>
-	</head>
+	$lang = substr($_SERVER["HTTP_ACCEPT"],0,2);
+	$whitelist = ["en", "fr"];
+	$d_lang = (in_array($lang, $whitelist)) ? $lang : "en";
 
-	<body style='width:100%; margin:0;'>
-		<div class='row justify-content-center align-items-center w-100 p-0 m-0 mt-4'>
+	switch($d_lang) {
+		case "en":
+			header("location:index_en.php");
+			break;
 
-			<?php include("head.php"); ?>
-
-			<div class='d-lg-flex d-none col-12 ' style='height:5em;width:100vw;'>
-			</div>
-
-			<?php include("projects.php"); ?>
-			<?php include("placeToLearn.php"); ?>
-		</div>
+		case "fr":
+			header("location:index_fr.php");
+			break;
 		
+	}
 
-
-
-
-		<!-- Bootstrap --!>
-		<script src='https://code.jquery.com/jquery-3.4.1.js'></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-			crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" 
-			crossorigin="anonymous"></script>
-		<script src='js/init.js'></script>
-		<script src='js/script.js'></script>
-	</body>
-</html>
+?>
